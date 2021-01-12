7cd06f826743b26f33b89707d93f6e50fc67bdaa
luckydipper <qhrqufdlek@khu.ac.kr>
2021-01-12 14:46:40 +0900

    [del] - useless code

--- c++/Project1/head.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-int sum(int a, int b);
--- c++/qt_creator/qt_creator.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    char k[30];
-    std::cout << "give me the sentence" << std::endl;
-    std::cin >> k;
-    std::cout << "hello world" << std::endl;
-}
